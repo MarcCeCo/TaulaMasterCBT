4ffc0727c35975fcc7c66a8a2d41ff105ef7e386
MarcCeCo <mcenteno@besos-tordera.cat>
2026-05-11 10:55:12 +0200

    Add files via upload

--- api/create-user.ts
+++ api/create-user.ts
@@ -56,7 +56,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     email,
     {
       data: { full_name, role },
-      redirectTo: `${process.env.VITE_APP_URL ?? ALLOWED_ORIGIN}/?invited=true`,
+      redirectTo: process.env.APP_URL ?? "https://taula-master-cbt.vercel.app",
     }
   );
 
